8a2b10373f9177ebcbaa7dd2203fd8e7d097af94
Hugo Abreu Mendes <hugoabreu1002@gmail.com>
2021-01-26 23:39:16 -0300

    todos updated

--- EnsembleSearch.py
+++ EnsembleSearch.py
@@ -30,8 +30,7 @@ class EnsembleSearch:
         self.alpha_stop = alpha_stop
 
     def gen_population(self):
-        # TODO 
-        # review this method
+        # TODO review this method
         # population could be an atribute, or a list of instance of some individual class.
         # where one individual would be a list o regressors.
 
@@ -41,8 +40,7 @@ class EnsembleSearch:
             
             qt_regressor = np.random.randint(2,9)
 
-            # TODO
-            # add parameters to LR
+            # TODO add parameters to LR
             # take a look at https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html?highlight=linearregression#sklearn.linear_model.LinearRegression
             lista_LR = ['LR',LR(), {}]
             
@@ -53,8 +51,7 @@ class EnsembleSearch:
                           'min_samples_leaf':np.random.randint(2,10),   
                           'min_weight_fraction_leaf':np.random.rand(1)[0]/2}]
             
-            # TODO
-            # Consider to remove ou replace, becouse is too slow
+            # TODO Consider to remove ou replace SVR, becouse is too slow
             lista_SVR = ['SVR',SVR(),
                          {'kernel':random.choice(['linear','rbf','poly','sigmoid']),     
                           'epsilon':np.random.rand(1)[0]/4,
@@ -78,8 +75,7 @@ class EnsembleSearch:
                          {'C': np.random.randint(1,10), 'early_stopping':True,        
                           'n_iter_no_change':np.random.randint(1,10)}]
             
-            # TODO 
-            # add parameters to SGD 
+            # TODO add parameters to SGD 
             # take a look at https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.SGDRegressor.html#sklearn.linear_model.SGDRegressor
             # consider to implement with https://scikit-learn.org/stable/modules/generated/sklearn.kernel_approximation.Nystroem.html#sklearn.kernel_approximation.Nystroem transformer
             # it would become a pipiline then?
@@ -164,8 +160,6 @@ class EnsembleSearch:
         return to_break
 
     def search_best(self):
-        # TODO
-        # it would be nicer to test each regressor from the ensemble individually too and add this information to the final choice.
         population = self.gen_population()
         population = self.set_fitness(population)
         population.sort(key = lambda x: x[-1])  
